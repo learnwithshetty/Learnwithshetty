cc4e106f2975e1d72e302c95b5b6a43acc527711
learnwithshetty <rohithshettytesting@gmail.com>
2025-03-09 12:28:34 +0530

    Okay

---
+++ Restassureed/src/test/java/rest1/ASCII.java
@@ -0,0 +1,18 @@
+package rest1;
+
+public class ASCII {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		char ch = '}';
+
+        int ascii = ch;
+
+        System.out.println("The ASCII value of " + ch
+                           + " is: " + ascii);
+    }
+
+	}
+
+
